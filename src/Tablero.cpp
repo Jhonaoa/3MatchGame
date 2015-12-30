226280225e956a17a0f33e1e959676cc2c936221
dbueno96 <daniel.bueno@correounivalle.edu.co>
2015-12-30 15:30:47 -0500

    Update Tablero.cpp

--- src/Tablero.cpp
+++ src/Tablero.cpp
@@ -54,6 +54,60 @@ void Tablero::getTableroInicial()
     cout <<tablero[8][8];
 }
 
+/* tablero::leerTableroDesdeTexto() : a partir de una archivo, imprime una cuadrícula
+ * de 8x8. */
+void Tablero::leerTableroDesdeTexto()
+{
+	ifstream nivel1 ("nivel1.txt"); //TIPO: ifstream, Nombre de Variable: Nivel1. 
+	//Archivo Buscado: nivel1.txt
+
+	if (nivel1.fail() ) // Si hay error al abrir el archivo entonces imprimir mensaje de error.
+	{
+		cerr << "ERROR AL ABRIR ARCHIVO" ;
+		cout << endl; 
+		exit(1);
+	}
+
+
+	
+	int numero[64];
+
+	int *puntero = NULL;
+	
+	for (int i = 0 ; i < 64 ; i++) // Ciclo FOR para guardar cada número de la matriz
+		//en un arreglo.
+
+	{
+		nivel1 >> numero[i] ; // Guarda lo que haya en el archivo  nivel1.txt en
+		//la posición i del arreglo numero[]. 
+
+	/* trataba de instanciar las cajas desde aquí, pero me tira error diciendo
+	 * algo como que se intenta hacer conversión de un puntero de caja a una caja 
+	 * de tipo no-escalar -btw no entendí porqué -
+
+		 Caja caja = new Caja();
+
+		vectorDeCajas[i] = caja;
+		
+	*/
+		
+	}
+
+	for (int i = 0 ; i < 64 ; i++)//Ciclo FOR para imprimir las componente del arreglo
+		//numero[] y formar una cuadrícula de números.
+	{
+		if ( i % 8 == 0 && i != 0) // si la fila es divisible por 8, se imprime un salto de línea.
+			cout << endl;
+		
+		
+		puntero  = &numero[i]; //puntero apunta a la dirección en memoria de la componente i del arreglo.
+		cout << (*puntero) << " ";  // se imprime lo señalado por el puntero.
+
+		
+	}
+
+
+}
 
 
 
