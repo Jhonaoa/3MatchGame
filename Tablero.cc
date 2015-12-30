6b8d0fde59b5ca46222f21118dc168ac86415a73
JhoanSebastian <sebas_tian11_@hotmail.com>
2015-12-30 18:15:34 -0500

    Update Tablero.cc

--- Tablero.cc
+++ Tablero.cc
@@ -67,18 +67,22 @@ void Tablero::hacerMatch()
 			for (columnas = 0; columnas < 8; columnas++)
 			{
 				puntero = &miTablero[filas][columnas];// Amarro el puntero a la posición de la matriz
-				// El if compara el valor donde se encuentra el puntero con 
+				// El if compara el valor donde se encuentra el puntero con el valor de arriba y el de abajo. 
 				if(*puntero == miTablero[filas + 1][columnas] and *puntero == miTablero[filas - 1][columnas])
 				{
-					*puntero = 0;
-					puntero = &miTablero[filas + 1][columnas];
-					*puntero = 0;
-					puntero = &miTablero[filas - 1][columnas];
-					*puntero = 0;
+					*puntero = 0; // Si esto sucede el lugar donde está el puntero toma el valor de 0
+					puntero = &miTablero[filas + 1][columnas]; //Muevo el puntero arriba
+					*puntero = 0; // cambio a 0
+					puntero = &miTablero[filas - 1][columnas]; // Muevo el puntero abajo
+					*puntero = 0; //cambio a 0
 				}
-				if(*puntero == miTablero[filas][columnas + 1] and *puntero == miTablero[filas][columnas - 1])
+				if(*puntero == miTablero[filas][columnas + 1] and *puntero == miTablero[filas][columnas - 1]) 
+				// ahora comparo con los lados derecha e izquierda
 				{
-					if (miTablero[filas][columnas + 1] != miTablero[filas + 1] [0] and miTablero[filas][columnas - 1] != miTablero[filas - 1] [7])
+					if (miTablero[filas][columnas + 1] != miTablero[filas + 1] [0] and miTablero[filas][columnas - 1] != miTablero[filas - 1] [7]) 
+					//este if es necesario para que el programa no hiciera combinaciones saltando de linea, ya que si se hace columna +1 en la 
+					// ultima columna se comparaba con el primer valor de la fila siguiente y si se hacia columna -1 en la primera columna, 
+					// Se comparaba con el ultimo valor de la fila anterior.
 					{
 					*puntero = 0;
 					puntero = &miTablero[filas][columnas - 1];
