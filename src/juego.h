13845e7739e3777ffe79ceb03bbf72dbbf07e667
Jhon Alejandro Orobio <alejoar76@hotmail.com>
2016-01-14 15:34:04 -0500

    Update juego.h

--- src/juego.h
+++ src/juego.h
@@ -1,4 +1,24 @@
-	 /*
+
+ */
+
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * juego.h
+ * Copyright (C) 2016 Jhon Alejandro Orobio <alejo@alejo-OEM>
+ *
+ * 3matchgameubuntu is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * 3matchgameubuntu is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 	 /*
 	 Clase: Juego: 
 	 Responsabilidad: mostrar las caracteristica que componen un juego. un juego contará con
 		 un nivel
@@ -13,6 +33,13 @@
 #ifndef _JUEGO_H_
 #define _JUEGO_H_
 
+#include <cstdlib>
+#include <iostream>
+#include <unistd.h>
+#include <time.h>
+
+ 
+using namespace std;
 class Juego
 {
 public: 
@@ -28,12 +55,16 @@ public:
 	void disminuirRequerimientoTipo2(); // reducen en una unidad, los requerimientos
 	void disminuirRequerimientoTipo3(); // de algún tipo, para terminar el nivel.
 	void disminuirRequerimientoTipo4();
-
+	void reloj();
 	int getNivel();
+	int getTiempo();
+	void setTiempo(int segundos);
 	int getRequerimientoTipo1();
 	int getRequerimientoTipo2();
 	int getRequerimientoTipo3();
 	int getRequerimientoTipo4();
+	bool requerimientosCumplidos ();
+	
 	
 
 	
@@ -46,8 +77,7 @@ private:
 	int requerimientoTipo3;
 	int requerimientoTipo4;
 	int nivel; 
-	// TIEMPO: NO SÉ DE QUÉ TIPO DEFINIRLO.
+	int tiempo; 
 	
 };
-
 #endif // _JUEGO_H_
